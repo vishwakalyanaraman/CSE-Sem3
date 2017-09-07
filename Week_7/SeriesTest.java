eef732f8a9cfbcb658bba09b2121743679c15047
vishwakalyanaraman <vishwa.kalyanaraman@gmail.com>
2017-09-07 16:05:42 +0530

    Add files via upload

---
+++ Week_7/SeriesTest.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+
+interface Series{
+	int getNext();
+	void reset();
+	void setStart(int a);
+}
+class ByTwos implements Series{
+	int x;
+	System
+	System.out.println("Enter the starting element of the series");
+}
